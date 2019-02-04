6ab32f3aa3a9a131cb335cb4bc60fa3d47c5201a
alex <sheremet-a@mail.ru>
2019-02-04 17:45:08 +0200

    Some changes in analyzer.

--- analyzer/weights.py
+++ analyzer/weights.py
@@ -1,7 +1,9 @@
 import random
+import spacy
 
+nlp = spacy.load('en')
 
-class WeightedListAnalyzer:
+class NodesBertScoring:
     def __init__(self):
         pass
 
@@ -11,3 +13,17 @@ class WeightedListAnalyzer:
         for item in text_list:
             result.append((item, random.randint(1, 101)))
         return result
+
+
+class EntityNodesFinder:
+    def __init__(self):
+        all_nps = None
+        all_vps = None
+        all_ners = None
+        all_tokens = None
+
+    def preprocess(self, raw_text):
+        doc = nlp(raw_text)
+        self.all_tokens = [token.text for token in doc]
+        self.all_nps = [(e.start, e.end) for e in doc.noun_chunks]
+        self.all_ners = [(e.start, e.end) for e in doc.ents]
